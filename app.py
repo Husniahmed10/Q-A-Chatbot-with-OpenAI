2d0305638af5e0b79e1627bd8782a5c3925604be
Husni Ahamed <141121519+Husniahmed10@users.noreply.github.com>
2025-08-24 15:11:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st 
+import openai
+from langchain_openai import ChatOpenAI 
+from langchain_core.output_parsers import StrOutputParser 
+from langchain_core.prompts import ChatPromptTemplate 
+
+import os 
+from dotenv import load_dotenv 
+load_dotenv()
+
+## Langsmith Tracking 
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_PROJECT'] = "Q&A Chatbot with OPENAI"
+
+## Prompt template 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a super helpful assistant like a pro. please response creatively and accurately for user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+def generate_response(question, api_key, llm, temperature, max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=llm, temperature=temperature, max_tokens=max_tokens)
+    outputparser = StrOutputParser()
+    chain = prompt|llm|outputparser
+    answer = chain.invoke({'question':question})
+    return answer
+
+## Title of the APP 
+st.title("Q&A Chatbot with OpenAI")
+
+## Sidebar for settings 
+st.sidebar.title("Settings")
+api_key = st.sidebar.text_input("Enter Your Open AI API Key: ", type="password")
+
+## Drop down to select various OpenAI Models 
+llm = st.sidebar.selectbox("Select an OpenAI Model", ["gpt-4o", "gpt-4-turbo", "gpt-4"])
+
+## Adjust response parameter 
+temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+
+## Main Interface for user input
+st.write("Go Ahead and ask any question")
+user_input = st.text_input("You: ")
+
+if user_input and api_key:
+    response = generate_response(user_input, api_key, llm, temperature, max_tokens)
+    st.write(response)
+
+elif user_input:
+    st.warning("Please Enter the OpenAI Api Key")
+
+else:
+    st.write("Please Provide the query")
